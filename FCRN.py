3fda5c202b8bf2c74b672019c480e696270dc6a1
CodingWang <wangxin_buaa@163.com>
2018-11-22 19:00:03 +0800

    add upsample in original paper

--- FCRN.py
+++ FCRN.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2018/11/19 18:00
+# @Time    : 2018/11/22 12:33
 # @Author  : Wang Xin
 # @Email   : wangxin_buaa@163.com
-
 
 import torch
 import torch.nn as nn
@@ -29,92 +28,172 @@ def weights_init(m):
         m.bias.data.zero_()
 
 
-class UpSample(nn.Module):
+class Unpool(nn.Module):
+    # Unpool: 2*2 unpooling with zero padding
+    def __init__(self, num_channels, stride=2):
+        super(Unpool, self).__init__()
 
-    def __init__(self, in_channel):
-        super(UpSample, self).__init__()
+        self.num_channels = num_channels
+        self.stride = stride
 
-        self.conv1_ = nn.Sequential(collections.OrderedDict([
-            ('conv1', nn.Conv2d(in_channel, in_channel // 2, kernel_size=3)),
-            ('bn1', nn.BatchNorm2d(in_channel // 2)),
-            ('relu', nn.ReLU(inplace=True))
-        ]))
-
-        self.conv2_ = nn.Sequential(collections.OrderedDict([
-            ('conv1', nn.Conv2d(in_channel, in_channel // 2, kernel_size=(2, 3))),
-            ('bn1', nn.BatchNorm2d(in_channel // 2)),
-            ('relu', nn.ReLU(inplace=True))
-        ]))
-
-        self.conv3_ = nn.Sequential(collections.OrderedDict([
-            ('conv1', nn.Conv2d(in_channel, in_channel // 2, kernel_size=(3, 2))),
-            ('bn1', nn.BatchNorm2d(in_channel // 2)),
-            ('relu', nn.ReLU(inplace=True))
-        ]))
-
-        self.conv4_ = nn.Sequential(collections.OrderedDict([
-            ('conv1', nn.Conv2d(in_channel, in_channel // 2, kernel_size=2)),
-            ('bn1', nn.BatchNorm2d(in_channel // 2)),
-            ('relu', nn.ReLU(inplace=True))
-        ]))
-
-        self.ps = nn.PixelShuffle(2)
-        self.relu = nn.ReLU()
+        # create kernel [1, 0; 0, 0]
+        self.weights = torch.autograd.Variable(
+            torch.zeros(num_channels, 1, stride, stride).cuda())  # currently not compatible with running on CPU
+        self.weights[:, :, 0, 0] = 1
 
     def forward(self, x):
-        x1 = self.conv1_(nn.functional.pad(x, (1, 1, 1, 1)))
-        x2 = self.conv2_(nn.functional.pad(x, (1, 1, 0, 1)))
-        x3 = self.conv3_(nn.functional.pad(x, (0, 1, 1, 1)))
-        x4 = self.conv4_(nn.functional.pad(x, (0, 1, 0, 1)))
-        x = torch.cat((x1, x2, x3, x4), dim=1)
-
-        output = self.ps(x)
-        output = self.relu(output)
-
-        return output
+        return F.conv_transpose2d(x, self.weights, stride=self.stride, groups=self.num_channels)
 
 
-class ASPP_module(nn.Module):
-    def __init__(self, inplanes, planes, rate):
-        super(ASPP_module, self).__init__()
-        if rate == 1:
-            kernel_size = 1
-            padding = 0
-        else:
-            kernel_size = 3
-            padding = rate
-        self.atrous_convolution = nn.Conv2d(inplanes, planes, kernel_size=kernel_size,
-                                            stride=1, padding=padding, dilation=rate, bias=False)
-        self.bn = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU()
+# class Unpool(nn.Module):
+#     '''
+#     Unpooling Layer implemented following description in
+#     A. Dosovitskiy, J. Tobias Springberg, and T.Brox.
+#     <Learning to generate chairs with convolutional neural networks>
+#     '''
+#
+#     def __init__(self, use_gpu=True):
+#         super(Unpool, self).__init__()
+#         kernel = torch.FloatTensor([[1, 0], [0, 0]])
+#         kernel = kernel.unsqueeze(0)
+#         kernel = kernel.unsqueeze(0)
+#         if use_gpu:
+#             self.kernel = kernel.cuda()
+#         else:
+#             self.kernel = kernel
+#         return
+#
+#     def forward(self, inputs):
+#         in_batches = inputs.size()[0]
+#         in_channels = inputs.size()[1]
+#         inputs = inputs.view(
+#             in_batches * in_channels,
+#             1, inputs.size()[2], inputs.size()[3]
+#         )
+#         inputs = torch.nn.functional.conv_transpose2d(
+#             inputs, self.kernel, stride=2
+#         )
+#         inputs = inputs.view(
+#             in_batches, in_channels, inputs.size()[2], inputs.size()[3]
+#         )
+#         return inputs
 
-        self._init_weight()
+
+class Decoder(nn.Module):
+    # Decoder is the base class for all decoders
+
+    names = ['deconv2', 'deconv3', 'upconv', 'upproj']
+
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.layer1 = None
+        self.layer2 = None
+        self.layer3 = None
+        self.layer4 = None
 
     def forward(self, x):
-        x = self.atrous_convolution(x)
-        x = self.bn(x)
-
-        return self.relu(x)
-
-    def _init_weight(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                # n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                # m.weight.data.normal_(0, math.sqrt(2. / n))
-                torch.nn.init.kaiming_normal_(m.weight)
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        return x
 
 
-class FCRN(nn.Module):
-    def __init__(self, layers = 50, output_size = (228, 304), in_channels=3, pretrained=True):
+class DeConv(Decoder):
+    def __init__(self, in_channels, kernel_size):
+        assert kernel_size >= 2, "kernel_size out of range: {}".format(kernel_size)
+        super(DeConv, self).__init__()
+
+        def convt(in_channels):
+            stride = 2
+            padding = (kernel_size - 1) // 2
+            output_padding = kernel_size % 2
+            assert -2 - 2 * padding + kernel_size + output_padding == 0, "deconv parameters incorrect"
+
+            module_name = "deconv{}".format(kernel_size)
+            return nn.Sequential(collections.OrderedDict([
+                (module_name, nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size,
+                                                 stride, padding, output_padding, bias=False)),
+                ('batchnorm', nn.BatchNorm2d(in_channels // 2)),
+                ('relu', nn.ReLU(inplace=True)),
+            ]))
+
+        self.layer1 = convt(in_channels)
+        self.layer2 = convt(in_channels // 2)
+        self.layer3 = convt(in_channels // (2 ** 2))
+        self.layer4 = convt(in_channels // (2 ** 3))
+
+
+class UpConv(Decoder):
+    # UpConv decoder consists of 4 upconv modules with decreasing number of channels and increasing feature map size
+    def upconv_module(self, in_channels):
+        # UpConv module: unpool -> 5*5 conv -> batchnorm -> ReLU
+        upconv = nn.Sequential(collections.OrderedDict([
+            ('unpool', Unpool(in_channels)),
+            ('conv', nn.Conv2d(in_channels, in_channels // 2, kernel_size=5, stride=1, padding=2, bias=False)),
+            ('batchnorm', nn.BatchNorm2d(in_channels // 2)),
+            ('relu', nn.ReLU()),
+        ]))
+        return upconv
+
+    def __init__(self, in_channels):
+        super(UpConv, self).__init__()
+        self.layer1 = self.upconv_module(in_channels)
+        self.layer2 = self.upconv_module(in_channels // 2)
+        self.layer3 = self.upconv_module(in_channels // 4)
+        self.layer4 = self.upconv_module(in_channels // 8)
+
+
+class UpProj(Decoder):
+    # UpProj decoder consists of 4 upproj modules with decreasing number of channels and increasing feature map size
+
+    class UpProjModule(nn.Module):
+        # UpProj module has two branches, with a Unpool at the start and a ReLu at the end
+        #   upper branch: 5*5 conv -> batchnorm -> ReLU -> 3*3 conv -> batchnorm
+        #   bottom branch: 5*5 conv -> batchnorm
+
+        def __init__(self, in_channels):
+            super(UpProj.UpProjModule, self).__init__()
+            out_channels = in_channels // 2
+            self.unpool = Unpool(in_channels)
+            self.upper_branch = nn.Sequential(collections.OrderedDict([
+                ('conv1', nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2, bias=False)),
+                ('batchnorm1', nn.BatchNorm2d(out_channels)),
+                ('relu', nn.ReLU()),
+                ('conv2', nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)),
+                ('batchnorm2', nn.BatchNorm2d(out_channels)),
+            ]))
+            self.bottom_branch = nn.Sequential(collections.OrderedDict([
+                ('conv', nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2, bias=False)),
+                ('batchnorm', nn.BatchNorm2d(out_channels)),
+            ]))
+            self.relu = nn.ReLU()
+
+        def forward(self, x):
+            x = self.unpool(x)
+            x1 = self.upper_branch(x)
+            x2 = self.bottom_branch(x)
+            x = x1 + x2
+            x = self.relu(x)
+            return x
+
+    def __init__(self, in_channels):
+        super(UpProj, self).__init__()
+        self.layer1 = self.UpProjModule(in_channels)
+        self.layer2 = self.UpProjModule(in_channels // 2)
+        self.layer3 = self.UpProjModule(in_channels // 4)
+        self.layer4 = self.UpProjModule(in_channels // 8)
+
+
+class ResNet(nn.Module):
+    def __init__(self, layers, output_size=(228, 304), in_channels=3, pretrained=True):
 
         if layers not in [18, 34, 50, 101, 152]:
             raise RuntimeError(
                 'Only 18, 34, 50, 101, and 152 layer model are defined for ResNet. Got {}'.format(layers))
 
-        super(FCRN, self).__init__()
+        super(ResNet, self).__init__()
         pretrained_model = torchvision.models.__dict__['resnet{}'.format(layers)](pretrained=pretrained)
 
         if in_channels == 3:
@@ -147,10 +226,12 @@ class FCRN(nn.Module):
         self.conv2 = nn.Conv2d(num_channels, num_channels // 2, kernel_size=1, bias=False)
         self.bn2 = nn.BatchNorm2d(num_channels // 2)
 
-        self.up1 = UpSample(num_channels // 2)
-        self.up2 = UpSample(num_channels // (2 ** 2))
-        self.up3 = UpSample(num_channels // (2 ** 3))
-        self.up4 = UpSample(num_channels // (2 ** 4))
+        # self.up1 = UpSample(num_channels // 2)
+        # self.up2 = UpSample(num_channels // (2 ** 2))
+        # self.up3 = UpSample(num_channels // (2 ** 3))
+        # self.up4 = UpSample(num_channels // (2 ** 4))
+
+        self.upSample = UpConv(num_channels // 2)
 
         # setting bias=true doesn't improve accuracy
         self.conv3 = nn.Conv2d(num_channels // 32, 1, kernel_size=3, stride=1, padding=1, bias=False)
@@ -160,10 +241,11 @@ class FCRN(nn.Module):
         self.conv2.apply(weights_init)
         self.bn2.apply(weights_init)
 
-        self.up1.apply(weights_init)
-        self.up2.apply(weights_init)
-        self.up3.apply(weights_init)
-        self.up4.apply(weights_init)
+        # self.up1.apply(weights_init)
+        # self.up2.apply(weights_init)
+        # self.up3.apply(weights_init)
+        # self.up4.apply(weights_init)
+        self.upSample.apply(weights_init)
 
         self.conv3.apply(weights_init)
 
@@ -178,26 +260,28 @@ class FCRN(nn.Module):
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
 
-        print('x4 size = ', x4.size())
-
         x = self.conv2(x4)
         x = self.bn2(x)
 
         # 上采样
-        x = self.up1(x)
-        x = self.up2(x)
-        x = self.up3(x)
-        x = self.up4(x)
+        # x = self.up1(x)
+        # x = self.up2(x)
+        # x = self.up3(x)
+        # x = self.up4(x)
+        x = self.upSample(x)
 
         x = self.conv3(x)
         x = self.bilinear(x)
 
         return x
 
+
 if __name__ == "__main__":
-    model = FCRN(layers=50, output_size=((228, 304)))
+    model = ResNet(layers=50, output_size=((228, 304)))
+    model = model.cuda()
     model.eval()
     image = torch.randn(1, 3, 228, 304)
+    image = image.cuda()
     with torch.no_grad():
-        output = model.forward(image)
+        output = model(image)
     print(output.size())
